d6bebffc66e4fabd3ced5a1441ac7f7d85eb1d9b
luigi_serra91@hotmail.it <luigi_serra91@hotmail.it>
2024-10-17 16:02:35 +0200

    fix_ciclo_for

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,32 @@
 const listEmail = document.querySelector('.list-email')
 
-axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
-  .then(response => {
-    const mails = response.data;
+
+
+  for (let i = 0; i < 10; i++){
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+    .then(response => {
+      const mails = response.data;
+  
+      console.log(mails);
+      let email = mails.response
+    
+    console.log(email);
+    
+    let markup = `
+    
+       <li>${email}</li>
+        
+    `
+  
+    
+    listEmail.innerHTML += markup
+  
+  
+    })
+
+
+
 
     
-
-
-    for (let i = 0; i < 10; i++){
-        let email = mails.response
-        console.log(email);
-        
-        let markup = `
-        
-           <li>${email}</li>
-            
-        `
-      
-        
-      listEmail.innerHTML += markup
-      
-      }
-
-  })
+  
+  }
